30127543f401316dde32d02b8854541a757bdde6
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-08-23 11:22:36 -0400

    login.js component complete, added private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
+
+const PrivateRoute = ({component: Component, ...rest}) => {
+  return <Route {...rest} render={props => {
+    if (localStorage.getItem(`token`)) {
+      return <Component {...props} />;
+    } else {
+      return <Redirect to="/login" />
+    }
+  }} />
+};
 
 function App() {
   const [colorList, setColorList] = useState([]);
@@ -10,10 +21,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        {<PrivateRoute exact path="/bubblepage" component={BubblePage} />}
       </div>
     </Router>
   );
